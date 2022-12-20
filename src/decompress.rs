0102aa262f5a66deb1d21c7fed1fd6193b74e851
Jonathan Lainer <jonathan@lainer.co.at>
2022-12-20 10:37:22 +0100

    Add tests for C.SW and C.SD.

--- src/decompress.rs
+++ src/decompress.rs
@@ -18,7 +18,7 @@ pub fn decompress_q0(i: u16) -> DecompressionResult {
         0b010 => decompress_load(i, CiInstr::Lw),
         0b011 => decompress_load(i, CiInstr::Ld),
         0b100 => Err(DecodingError::Reserved),
-        0b101 => Err(DecodingError::Unimplemented), // C.FSD, RV32FC only instrcution
+        0b101 => Err(DecodingError::Unimplemented), // C.FSD
         0b110 => decompress_store(i, CsInstr::Sw),
         0b111 => decompress_store(i, CsInstr::Sd),
         _ => unreachable!(),
@@ -65,6 +65,7 @@ mod tests {
         assert_eq!(decode(0x002c).unwrap(), Addi(IType(0x00810593))); // addi a1, sp, 8
 
         // C.LFD unimplemented
+
         // C.LW
         assert_eq!(decode(0x4298).unwrap(), Lw(IType(0x0006a703))); // lw a4, 0(a3)
         assert_eq!(decode(0x483c).unwrap(), Lw(IType(0x05042783))); // lw a5, 80(s0)
@@ -72,5 +73,17 @@ mod tests {
         // C.LD
         assert_eq!(decode(0x6398).unwrap(), Ld(IType(0x0007b703))); // ld a4, 0(a5)
         assert_eq!(decode(0x6b9c).unwrap(), Ld(IType(0x0107b783))); // ld a5, 16(a5)
+
+        // Reserved
+
+        // C.FSD unimplemented
+
+        // C.SW
+        assert_eq!(decode(0xc298).unwrap(), Sw(SType(0x00e6a023))); // sw a4, 0(a3)
+        assert_eq!(decode(0xd01c).unwrap(), Sw(SType(0x02f42023))); // sw a5, 32(s0)
+
+        // C.SD
+        assert_eq!(decode(0xe398).unwrap(), Sd(SType(0x00e7b023))); // sd a4, 0(a5)
+        assert_eq!(decode(0xee98).unwrap(), Sd(SType(0x00e6bc23))); // sd a4, 24(a3)
     }
 }
